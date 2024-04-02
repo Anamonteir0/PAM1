7517427eb0be2a1a116845211a8b8cfc3626d198
Ana Monteiro <140262688+Anamonteir0@users.noreply.github.com>
2024-04-01 22:24:16 -0300

    Variaveis

---
+++ variaveis.js
@@ -0,0 +1,8 @@
+var variavel_1=2;
+let variavel_2=5;
+const variavel_3= "Olá mundo";
+
+console.log(variavel_3);
+console.log("olá mundo!");
+console.log("mensagem", variavel_3);
+console.log("mensagem"+ variavel_3);
